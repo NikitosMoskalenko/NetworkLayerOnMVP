a27638455f9f93b1ef97c4da5d9f9f6436de9ddd
Moskalenko Nikita <n.moskalneko1996@yahoo.com>
2020-07-15 20:08:12 +0300

    Initial Commit

---
+++ TestPryaniky/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestPryaniky
+//
+//  Created by Nikita Moskalenko on 7/15/20.
+//  Copyright © 2020 Nikita Moskalenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
